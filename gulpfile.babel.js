199741ad1b29acd1e1b85ccbf90c63adfd8daac0
insight88 <kpkim1988@gmail.com>
2020-08-19 14:12:25 +0900

    Deploy on github.io

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,9 @@ import image from "gulp-image";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 sass.compiler = require("node-sass");
 
@@ -24,12 +27,17 @@ const routes = {
         src: "src/scss/style.scss",
         dest: "build/css",
     },
+    js: {
+        watch: "src/js/**/*.js",
+        src: "src/js/main.js",
+        dest: "build/js",
+    },
 };
 
 const pug = () =>
     gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build/"]);
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () =>
     gulp.src("build").pipe(
@@ -43,26 +51,50 @@ const img = () =>
 
 const style = () =>
     gulp
-        .src(routes.scss.src)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(
-            autoprefixer({
-                development: ["last 2 versions"],
-            })
-        )
-        .pipe(miniCSS())
-        .pipe(gulp.dest(routes.scss.dest));
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+        autoprefixer({
+            development: ["last 2 versions"],
+        })
+    )
+    .pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
+
+const js = () =>
+    gulp
+    .src(routes.js.src)
+    .pipe(
+        bro({
+            transform: [
+                babelify.configure({
+                    presets: ["@babel/preset-env"]
+                }),
+                ["uglifyify", {
+                    global: true
+                }],
+            ],
+        })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
+const gh = () => gulp.src("build/**/*").pipe(ghPages());
 
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
     gulp.watch(routes.pug.src, img);
     gulp.watch(routes.scss.watch, style);
+    gulp.watch(routes.js.watch, js);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, style]);
+const assets = gulp.series([pug, style, js]);
 
 const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+
+export const dev = gulp.series([build, live]);
+
+export const deploy = gulp.series([build, gh, clean]);
