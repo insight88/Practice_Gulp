826fbd6eb08536a0e7c96f3000e32ad441f8fb61
insight88 <kpkim1988@gmail.com>
2020-08-19 12:29:01 +0900

    gulp sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,35 +2,59 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
-  pug: {
-    watch: "src/**/*.pug",
-    src: "src/*.pug",
-    dest: "build",
-  },
+    pug: {
+        watch: "src/**/*.pug",
+        src: "src/*.pug",
+        dest: "build",
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img",
+    },
+    scss: {
+        watch: "src/scss/**/*.scss",
+        src: "src/scss/style.scss",
+        dest: "build/css",
+    },
 };
 
 const pug = () =>
-  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
 const clean = () => del(["build/"]);
 
 const webserver = () =>
-  gulp.src("build").pipe(ws({
-    livereload: true,
-    open: true
-  }));
+    gulp.src("build").pipe(
+        ws({
+            livereload: true,
+        })
+    );
+
+const img = () =>
+    gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+
+const style = () =>
+    gulp
+        .src(routes.scss.src)
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest(routes.scss.dest));
 
 const watch = () => {
-  gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.pug.src, img);
+    gulp.watch(routes.scss.watch, style);
 };
 
+const prepare = gulp.series([clean, img]);
 
-const prepare = gulp.series([clean]);
+const assets = gulp.series([pug, style]);
 
-const assets = gulp.series([pug]);
+const live = gulp.parallel([webserver, watch]);
 
-const postDev = gulp.series([webserver, watch]);
-
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
