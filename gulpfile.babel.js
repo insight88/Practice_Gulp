8034311b1ad1e6a75cc5d4abc1e5e5e2de34e0b5
insight88 <kpkim1988@gmail.com>
2020-08-19 11:58:13 +0900

    webserver and watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,36 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
-    pug: {
-        src: "src/*.pug",
-        dest: "build",
-    },
+  pug: {
+    watch: "src/**/*.pug",
+    src: "src/*.pug",
+    dest: "build",
+  },
 };
 
 const pug = () =>
-    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
+
+const webserver = () =>
+  gulp.src("build").pipe(ws({
+    livereload: true,
+    open: true
+  }));
+
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
 
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver, watch]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
